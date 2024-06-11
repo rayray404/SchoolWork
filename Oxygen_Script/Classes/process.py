44af964d82f8e69f1005e7ef0c52a26946fc80ea
Rayhan <s2021173@iisjapan.com>
2024-06-11 15:49:02 +0900

    a

--- Oxygen_Script/Classes/process.py
+++ Oxygen_Script/Classes/process.py
@@ -1,7 +1,26 @@
+class Stack:
+    def __init__(self, stack):
+        self.stack = stack
+    def pop(self, condition=True):
+        if condition:
+            return(self.stack.pop())
+
+    def push(self, element):
+        self.stack.push()
+
 class Processor:
     def __init__(self):
-        pass
+        stacks = []
+        self.string_cache = dict()
+
+    def process(self, expression):
+        string_count = expression.count('"')
     
-    def process(self, cmd):
+        for i in range(string_count//2):
+            index1 = expression.index('"')
+            index2 = expression[index1+1:].index('"')
+            self.string_cache[len(self.string_cache)] = expression[index1+1:index2]
+            expression = expression[:index1]+f'{ {len(self.string_cache)} }'.replace(' ', '') + expression[index2+1:]
+            print(expression)
         
-        return eval(" ".join(cmd))
+        return eval(" ".join(expression))
--- Oxygen_Script/Classes/types.py
+++ Oxygen_Script/Classes/types.py
@@ -3,7 +3,10 @@ def conv_int(num):
     return int(num)
 def conv_abs(num):
     return abs(num)
-types = {'Z': (conv_int,), 'N': (conv_int, conv_abs)}
+def conv_float(num):
+    return float(num)
+
+types = {'Z': (conv_int,), 'N': (conv_int, conv_abs), 'Q': (conv_float,)}
 
 def type_cast(data, datatype):
     for i in types[datatype]:
